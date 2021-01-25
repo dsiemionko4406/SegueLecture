49911407317cdffe7ff393fead3c3de2854eca1a
David Siemionko <dsiemionko4406@stu.d214.org>
2021-01-25 14:58:44 -0600

    Added a view controller and a code for it.

---
+++ SegueLecture/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  SegueLecture
+//
+//  Created by David Siemionko on 1/25/21.
+//  Copyright © 2021 David Siemionko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
