9629ec9846106d90eb05259dae26e6d9dc99630b
David Siemionko <dsiemionko4406@stu.d214.org>
2021-01-25 14:50:40 -0600

    Initial Commit

---
+++ SegueLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegueLecture
+//
+//  Created by David Siemionko on 1/25/21.
+//  Copyright © 2021 David Siemionko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
